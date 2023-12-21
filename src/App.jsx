d74feaab81b5d4792bbe654148ee852e0feb26a7
Jose Andrés <oliver_ja@outlook.com>
2023-12-21 18:42:21 +0100

    feature: navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -4,23 +4,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import NavBar from './NavBar/NavBar';
 
 function App() {
   const [count, setCount] = useState(0);
-  
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+      <NavBar/>
       <h1>Vite + React</h1>
-      <div className="card">
+      <div className='col top-center'>
       <Button variant="contained" onClick={() => setCount((count) => count + 1)}>+1</Button>
         <p>
           count is {count}
@@ -29,9 +21,10 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
+      <p>
         Click on the Vite and React logos to learn more
       </p>
+      <div className='ocupa-mucho'> ocupa mucho </div>
     </>
   )
 }
---
+++ src/NavBar/NavBar.jsx
@@ -0,0 +1,79 @@
+import React from 'react';
+import './NavBar.css';
+
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import CssBaseline from '@mui/material/CssBaseline';
+import useScrollTrigger from '@mui/material/useScrollTrigger';
+import Slide from '@mui/material/Slide';
+import { List, ListItemButton, ListItemText, Fab, useMediaQuery } from '@mui/material';
+import { useTheme } from '@mui/material/styles';
+import KeyboardArrowUpIcon from '@mui/icons-material/ArrowUpward'
+
+const NavBar = (props) => {
+
+    const theme = useTheme();
+    const is_sm = useMediaQuery(theme.breakpoints.up('sm'));
+
+    function HideOnScroll(props) {
+        const { children, window } = props;
+        const trigger = useScrollTrigger({
+          target: window ? window() : undefined,
+        });
+      
+        return (
+          <Slide appear={false} direction="down" in={!trigger}>
+            {children}
+          </Slide>
+        );
+    }
+    const ShowOnScroll = (props) => {
+        const { children, window } = props;
+        const trigger = useScrollTrigger({
+            disableHysteresis: true,
+            threshold: 0,
+            target: window ? window() : undefined,
+          });
+
+        return (
+            <Slide appear={false} direction="up" in={trigger}>
+              {children}
+            </Slide>
+          );
+    }
+    
+  return (
+    <React.Fragment>
+        <CssBaseline />
+        <HideOnScroll {...props}>
+            <AppBar>
+                <Toolbar className={is_sm?"":"invisible"}>
+                        <List id='nav' component="nav" className='row menu'>
+                            <img id='logo' src={"logo_ico.ico"} className="logo" alt="logo" height={"70px"} />
+                            <ListItemButton id='home'>
+                                <ListItemText primary="Home" />
+                            </ListItemButton>
+                            <hr />
+                            <ListItemButton id='categorias'>
+                                <ListItemText primary="Categorias" />
+                            </ListItemButton>
+                        </List>
+                </Toolbar>
+                <Toolbar className={is_sm?"invisible":""}>
+                    <ListItemText primary="pequeño" />
+                </Toolbar>
+            </AppBar>
+        </HideOnScroll>
+        <ShowOnScroll {...props}>
+            
+        <Fab className='boton-sticky' size="small" aria-label="scroll back to top" onClick={()=>scrollTo(0,0)}>
+            <KeyboardArrowUpIcon />
+        </Fab>
+        </ShowOnScroll>
+    </React.Fragment>
+    
+  )
+}
+
+export default NavBar
